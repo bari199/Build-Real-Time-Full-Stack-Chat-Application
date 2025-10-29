c11ad22e0436089ab3def9cc5e63f8402d603a6f
bari@199 <bari1999smab@gmail.com>
2025-10-29 08:57:25 -0700

    Added Sidebar, RightSidebar, ChatContent pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,10 @@ import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </div>
